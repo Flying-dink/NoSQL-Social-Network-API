ef9716c8c7acde8b2a430c43be40143214710b79
gail enstice <skyedoll@aol.com>
2021-10-13 22:30:02 -0400

    added to to api routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/');
-
+const thoughtsRoutes= require('./thoughts-routes');
 router.use('/api', apiRoutes);
+router.use('/users',userRoutes);
+router.use('/thoughts',thoughtsRoutes);
 
 router.use((req,res) => {
     res.status(404).send('<h1>404 Error!<h1>');
